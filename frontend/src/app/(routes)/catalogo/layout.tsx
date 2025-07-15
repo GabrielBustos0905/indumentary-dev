9387ef3e4431d5714f8af18e9962c3532f1e5bb9
Gabriel Bustos <gabrielbustos0905@gmail.com>
2025-07-14 21:15:19 -0300

    feat: update some wachines

--- frontend/src/app/(routes)/catalogo/layout.tsx
+++ frontend/src/app/(routes)/catalogo/layout.tsx
@@ -1,13 +1,10 @@
-import { CatalogBreadcrumb, CatalogSearchBar, SideNavbar } from "@/components/Catalogo";
+import { CatalogBreadcrumb, SideNavbar } from "@/components/Catalogo";
 import { Separator } from "@/components/ui/separator";
 
 export default function CatalogoLayout({ children }: { children: React.ReactNode }) {
     return (
         <div className="my-4 px-12">
-            <div className="flex justify-between items-center py-2">
-                <CatalogBreadcrumb />
-                <CatalogSearchBar />
-            </div>
+            <CatalogBreadcrumb />
             <Separator />
             <div className="mt-4 flex justify-between gap-4">
                 <SideNavbar />
--- frontend/src/components/Catalogo/SideNavbar/SideNavbar.tsx
+++ frontend/src/components/Catalogo/SideNavbar/SideNavbar.tsx
@@ -1,8 +1,10 @@
+import { CatalogSearchBar } from "../CatalogSearchBar";
 import { FilterByType, PriceFilter, SizeFilter } from "./Filters";
 
 export function SideNavbar() {
     return (
         <nav className="w-[270px] flex flex-col h-screen items-start gap-4">
+            <CatalogSearchBar />
             <FilterByType />
             <SizeFilter />
             <PriceFilter />

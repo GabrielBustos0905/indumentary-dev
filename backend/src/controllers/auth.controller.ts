19e02adad419276227317b03aef08738bc810345
Gabriel Bustos <gabrielbustos0905@gmail.com>
2025-07-18 19:39:00 -0300

    feat: none

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -63,8 +63,10 @@ export const login = async (req: Request, res: Response): Promise<any> => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
+      // secure: process.env.NODE_ENV === 'production',
+      // sameSite: 'lax',
+      secure: process.env.NODE_ENV === 'production', // false en local
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 días
     })
 

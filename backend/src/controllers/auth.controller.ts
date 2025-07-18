56686da3ef541a729f5696c5994399504985f826
Gabriel Bustos <gabrielbustos0905@gmail.com>
2025-07-18 20:42:52 -0300

    some fixes

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -63,21 +63,21 @@ export const login = async (req: Request, res: Response): Promise<any> => {
 
     // console.log('user.userType:', user.userType)
 
-    // res.cookie('token', token, {
-    //   httpOnly: true,
-    //   // secure: process.env.NODE_ENV === 'production',
-    //   // sameSite: 'lax',
-    //   secure: process.env.NODE_ENV === 'production', // false en local
-    //   sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-    //   maxAge: 7 * 24 * 60 * 60 * 1000 // 7 días
-    // })
-
     res.cookie('token', token, {
       httpOnly: true,
-      secure: isProduction,
-      sameSite: isProduction ? 'none' : 'lax',
-      maxAge: 7 * 24 * 60 * 60 * 1000
+      // secure: process.env.NODE_ENV === 'production',
+      // sameSite: 'lax',
+      secure: process.env.NODE_ENV === 'production', // false en local
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+      maxAge: 7 * 24 * 60 * 60 * 1000 // 7 días
     })
+
+    // res.cookie('token', token, {
+    //   httpOnly: true,
+    //   secure: isProduction,
+    //   sameSite: isProduction ? 'none' : 'lax',
+    //   maxAge: 7 * 24 * 60 * 60 * 1000
+    // })
 
     return res.status(200).json({
       message: 'Login exitoso',
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -22,29 +22,29 @@ dotenv.config()
 const app = express()
 
 app.use(cookieParser())
-// app.use(session({
-//   secret: JWT_SECRET, // puede ser tu JWT_SECRET o uno propio
-//   resave: false,
-//   saveUninitialized: false,
-//   cookie: {
-//     httpOnly: true,
-//     secure: process.env.NODE_ENV === 'production',
-//     maxAge: 7 * 24 * 60 * 60 * 1000
-//   } // opcional, caducidad de la cookie de sesión
-// }))
-const isProduction = process.env.NODE_ENV === 'production'
-
 app.use(session({
-  secret: JWT_SECRET,
+  secret: JWT_SECRET, // puede ser tu JWT_SECRET o uno propio
   resave: false,
   saveUninitialized: false,
   cookie: {
     httpOnly: true,
-    secure: isProduction,
-    sameSite: isProduction ? 'none' : 'lax',
+    secure: process.env.NODE_ENV === 'production',
     maxAge: 7 * 24 * 60 * 60 * 1000
-  }
+  } // opcional, caducidad de la cookie de sesión
 }))
+// const isProduction = process.env.NODE_ENV === 'production'
+
+// app.use(session({
+//   secret: JWT_SECRET,
+//   resave: false,
+//   saveUninitialized: false,
+//   cookie: {
+//     httpOnly: true,
+//     secure: isProduction,
+//     sameSite: isProduction ? 'none' : 'lax',
+//     maxAge: 7 * 24 * 60 * 60 * 1000
+//   }
+// }))
 
 app.use(passport.initialize())
 app.use(passport.session())

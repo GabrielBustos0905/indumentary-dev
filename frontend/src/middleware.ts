1271275c78f90258d1c6cc7a8ec515af1c68ab28
Gabriel Bustos <gabrielbustos0905@gmail.com>
2025-07-22 11:19:32 -0300

    veamos

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,73 +1,73 @@
-// import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
 // import { authRoutes, publicRoutes, DEFAULT_LOGIN_REDIRECT } from './routes';
 
-// export function middleware(req: NextRequest) {
-//   const token = req.cookies.get('token_middleware')?.value;
-//   console.log(token)
-//   const { nextUrl } = req;
-//   const pathname = nextUrl.pathname;
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get('token_middleware')?.value;
+  console.log(token)
+  // const { nextUrl } = req;
+  // const pathname = nextUrl.pathname;
 
-//   // const isPublicRoute = publicRoutes.includes(pathname);
-//   // const isAuthRoute = authRoutes.includes(pathname);
+  // // const isPublicRoute = publicRoutes.includes(pathname);
+  // // const isAuthRoute = authRoutes.includes(pathname);
 
-//   // Decodificar el token para obtener el userType
-//   let userType: string | null = null;
+  // // Decodificar el token para obtener el userType
+  // let userType: string | null = null;
 
-//   if (token) {
-//     try {
-//       const payload = JSON.parse(atob(token.split('.')[1]));
-//       userType = payload.userType;
-//     } catch (err) {
-//       console.error('Token inválido', err);
-//     }
-//   }
+  // if (token) {
+  //   try {
+  //     const payload = JSON.parse(atob(token.split('.')[1]));
+  //     userType = payload.userType;
+  //   } catch (err) {
+  //     console.error('Token inválido', err);
+  //   }
+  // }
 
-//   // console.log({"user": userType})
+  // // console.log({"user": userType})
 
-//   // // 🔐 Si es ruta de autenticación y está logueado, redirigir
-//   // if (isAuthRoute && token) {
-//   //   return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-//   // }
+  // // // 🔐 Si es ruta de autenticación y está logueado, redirigir
+  // // if (isAuthRoute && token) {
+  // //   return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+  // // }
 
-//   // // 🔒 Si NO está logueado y NO es pública → redirigir al login
-//   // if (!token && !isPublicRoute) {
-//   //   const callbackUrl = pathname + nextUrl.search;
-//   //   return NextResponse.redirect(new URL(`/auth/login?callbackUrl=${encodeURIComponent(callbackUrl)}`, nextUrl));
-//   // }
+  // // // 🔒 Si NO está logueado y NO es pública → redirigir al login
+  // // if (!token && !isPublicRoute) {
+  // //   const callbackUrl = pathname + nextUrl.search;
+  // //   return NextResponse.redirect(new URL(`/auth/login?callbackUrl=${encodeURIComponent(callbackUrl)}`, nextUrl));
+  // // }
 
-//   // // 🚫 Si la ruta comienza con /admin y el userType no es admin
-//   // if (pathname.startsWith('/admin') && userType !== 'ADMIN') {
-//   //   return NextResponse.redirect(new URL('/unauthorized', nextUrl));
-//   // }
+  // // // 🚫 Si la ruta comienza con /admin y el userType no es admin
+  // // if (pathname.startsWith('/admin') && userType !== 'ADMIN') {
+  // //   return NextResponse.redirect(new URL('/unauthorized', nextUrl));
+  // // }
 
-//   // return NextResponse.next();
-//   const isPublicRoute = publicRoutes.includes(pathname);
-//   const isAuthRoute = authRoutes.includes(pathname);
+  // // return NextResponse.next();
+  // const isPublicRoute = publicRoutes.includes(pathname);
+  // const isAuthRoute = authRoutes.includes(pathname);
 
-//   if(isAuthRoute) {
-//     if(token) return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-//     return
-//   };
+  // if(isAuthRoute) {
+  //   if(token) return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+  //   return
+  // };
   
-//   if(!token && !isPublicRoute) {
-//     let callbackUrl = nextUrl.pathname;
+  // if(!token && !isPublicRoute) {
+  //   let callbackUrl = nextUrl.pathname;
 
-//     if(nextUrl.search) callbackUrl += nextUrl.search;
+  //   if(nextUrl.search) callbackUrl += nextUrl.search;
 
-//     const encodeCallbackUrl = encodeURIComponent(callbackUrl);
+  //   const encodeCallbackUrl = encodeURIComponent(callbackUrl);
 
-//     return Response.redirect(new URL(`/auth/login?callbackUrl=${encodeCallbackUrl}`, nextUrl));
-//   };
+  //   return Response.redirect(new URL(`/auth/login?callbackUrl=${encodeCallbackUrl}`, nextUrl));
+  // };
 
-//   if(pathname.startsWith("/admin") && userType !== "ADMIN") {
-//     return NextResponse.redirect(new URL('/unauthorized', nextUrl))
-//   }
+  // if(pathname.startsWith("/admin") && userType !== "ADMIN") {
+  //   return NextResponse.redirect(new URL('/unauthorized', nextUrl))
+  // }
 
-//   return
-// }
+  return
+}
 
-// export const config = {
-//   matcher: [
-//     '/((?!api|_next/static|_next/image|favicon.ico).*)',
-//   ],
-// };
+export const config = {
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+};

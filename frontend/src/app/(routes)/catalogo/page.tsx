0524c8b758e098eb2960add8e1199e73eb3bd06e
Gabriel Bustos <gabrielbustos0905@gmail.com>
2025-07-22 19:07:07 -0300

    feat lol

--- frontend/src/app/(routes)/catalogo/page.tsx
+++ frontend/src/app/(routes)/catalogo/page.tsx
@@ -1,6 +1,6 @@
-// import { ProductsGrid } from "@/components/Catalogo";
-// import { Loader } from "@/components/Loader";
-// import { fetchProducts } from "@/services/product.service";
+import { ProductsGrid } from "@/components/Catalogo";
+import { Loader } from "@/components/Loader";
+import { fetchProducts } from "@/services/product.service";
 
 // interface CatalogoPageProps {
 //     searchParams: {
@@ -30,23 +30,42 @@
 //     );
 // }
 
-import { ProductsGrid } from "@/components/Catalogo";
-import { Loader } from "@/components/Loader";
-import { fetchProducts } from "@/services/product.service";
+// import { ProductsGrid } from "@/components/Catalogo";
+// import { Loader } from "@/components/Loader";
+// import { fetchProducts } from "@/services/product.service";
 
-interface CatalogoPageProps {
-    searchParams: {
-        typeId?: string;
-        size?: string;
-        minPrice?: string;
-        maxPrice?: string;
-        q?: string; // search query
-        page?: string;
-    };
-}
+// interface CatalogoPageProps {
+//     searchParams: {
+//         typeId?: string;
+//         size?: string;
+//         minPrice?: string;
+//         maxPrice?: string;
+//         q?: string; // search query
+//         page?: string;
+//     };
+// }
 
-export default async function CatalogoPage({ searchParams }: { searchParams: Promise<CatalogoPageProps> }) {
-    const products = await fetchProducts((await searchParams).searchParams);
+// export default async function CatalogoPage({ searchParams }: { searchParams: Promise<CatalogoPageProps> }) {
+//     const products = await fetchProducts((await searchParams).searchParams);
+
+//     if (products.data.length === 0) return <Loader />;
+
+//     return (
+//         <div>
+//             <section>
+//                 <ProductsGrid products={products.data} />
+//             </section>
+//         </div>
+//     );
+// }
+
+export default async function CatalogoPage({
+    searchParams,
+}: {
+    searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}) {
+    const filters = await searchParams
+    const products = await fetchProducts(filters);
 
     if (products.data.length === 0) return <Loader />;
 
@@ -58,5 +77,6 @@ export default async function CatalogoPage({ searchParams }: { searchParams: Pro
         </div>
     );
 }
+
 
 

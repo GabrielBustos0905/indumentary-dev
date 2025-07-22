8706033060ffa40ff0bc31f2a851adfcad81dab9
Gabriel Bustos <gabrielbustos0905@gmail.com>
2025-07-22 18:07:37 -0300

    lol

--- frontend/src/app/(routes)/catalogo/page.tsx
+++ frontend/src/app/(routes)/catalogo/page.tsx
@@ -1,3 +1,35 @@
+// import { ProductsGrid } from "@/components/Catalogo";
+// import { Loader } from "@/components/Loader";
+// import { fetchProducts } from "@/services/product.service";
+
+// interface CatalogoPageProps {
+//     searchParams: {
+//         type?: string;
+//         size?: string;
+//         minPrice?: string;
+//         maxPrice?: string;
+//         q?: string; // search query
+//         page?: string;
+//     };
+// }
+
+// export default async function CatalogoPage({ params }: { params: Promise<CatalogoPageProps> }) {
+
+//     const products = await fetchProducts((await params).searchParams);
+
+//     if (products.data.length === 0) return <Loader />
+
+//     console.log(params)
+
+//     return (
+//         <div>
+//             <section>
+//                 <ProductsGrid products={products.data} />
+//             </section>
+//         </div>
+//     );
+// }
+
 import { ProductsGrid } from "@/components/Catalogo";
 import { Loader } from "@/components/Loader";
 import { fetchProducts } from "@/services/product.service";
@@ -13,13 +45,10 @@ interface CatalogoPageProps {
     };
 }
 
-export default async function CatalogoPage({ params }: { params: Promise<CatalogoPageProps> }) {
+export default async function CatalogoPage({ searchParams }: CatalogoPageProps) {
+    const products = await fetchProducts(searchParams);
 
-    const products = await fetchProducts((await params).searchParams);
-
-    if (products.data.length === 0) return <Loader />
-
-    console.log(params)
+    if (products.data.length === 0) return <Loader />;
 
     return (
         <div>
@@ -29,4 +58,5 @@ export default async function CatalogoPage({ params }: { params: Promise<Catalog
         </div>
     );
 }
+
 

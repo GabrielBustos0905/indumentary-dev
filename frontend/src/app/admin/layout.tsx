6a18baa8d712b8d8443e1c4fb1a963a316a06ff4
Gabriel Bustos <gabrielbustos0905@gmail.com>
2025-07-25 18:46:22 -0300

    lol

--- frontend/src/app/admin/layout.tsx
+++ frontend/src/app/admin/layout.tsx
@@ -23,84 +23,25 @@ export default function AdminLayout({
 }>) {
     return (
         <html lang="en">
-            <body
-                className={`${inter.className} antialiased`}
-            >
+            <body className={`${inter.className} antialiased`}>
                 <ProtectRoute allowedRoles={['ADMIN']}>
                     <SidebarProvider>
-                        <div className="flex min-h-screen">
-                            <AppSidebar />
-
-                            <div className="flex-1 flex">
-                                <SidebarInset>
-                                    <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-                                        <div className="flex items-center gap-2 px-4">
-                                            <SidebarTrigger className="-ml-1" />
-                                            <Separator orientation="vertical" className="mr-2 h-4" />
-                                        </div>
-                                    </header>
-                                </SidebarInset>
-
-                                <main className="flex-1 mt-8 px-4">
-                                    {children}
-                                    <Toaster />
-                                </main>
-                            </div>
-                        </div>
+                        <AppSidebar />
+                        <SidebarInset>
+                            <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+                                <div className="flex items-center gap-2 px-4">
+                                    <SidebarTrigger className="-ml-1" />
+                                    <Separator orientation="vertical" className="mr-2 h-4" />
+                                </div>
+                            </header>
+                        </SidebarInset>
+                        <main className="w-full mt-8">
+                            {children}
+                            <Toaster />
+                        </main>
                     </SidebarProvider>
-
                 </ProtectRoute>
             </body>
-        </html>
-    );
+        </html >
+    )
 }
-
-// import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
-// import "../globals.css";
-// import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-// import { Separator } from "@/components/ui/separator";
-// import { AppSidebar } from "@/components/Admin";
-// import { Toaster } from "@/components/ui/sonner";
-// import { ProtectRoute } from "@/components/ProtectRoute";
-
-// const inter = Inter({
-//     subsets: ["latin"],
-// });
-
-// export const metadata: Metadata = {
-//     title: "Indumentary Dev - Admin",
-//     description: "",
-// };
-
-// export default function AdminLayout({
-//     children,
-// }: Readonly<{
-//     children: React.ReactNode;
-// }>) {
-//     return (
-//         <html lang="en">
-//             <body
-//                 className={${inter.className} antialiased}
-//             >
-//                 <ProtectRoute allowedRoles={['ADMIN']}>
-//                     <SidebarProvider>
-//                         <AppSidebar />
-//                         <SidebarInset>
-//                             <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-//                                 <div className="flex items-center gap-2 px-4">
-//                                     <SidebarTrigger className="-ml-1" />
-//                                     <Separator orientation="vertical" className="mr-2 h-4" />
-//                                 </div>
-//                             </header>
-//                         </SidebarInset>
-//                         <main className="w-full mt-8">
-//                             {children}
-//                             <Toaster />
-//                         </main>
-//                     </SidebarProvider>
-//                 </ProtectRoute>
-//             </body>
-//         </html>
-//     );
-// }
